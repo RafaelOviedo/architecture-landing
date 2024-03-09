a10902591d9dacd3e898d48b49c116aed880a4a8
Rafael Oviedo <rafaeloviedo@Rafaels-Air.fibertel.com.ar>
2024-03-08 23:28:18 -0300

    update: updated navbar with responsive and added herosection component

---
+++ src/components/HeroSection/HeroSection.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <main class="hero-section-component">
+    Calculadora
+  </main>
+</template>
+
+<style>
+.hero-section-component {
+  width: 100%;
+  height: 50%;
+  border: 1px solid blue;
+}
+</style>
--- src/components/NavBar/NavBar.vue
+++ src/components/NavBar/NavBar.vue
@@ -1,44 +1,156 @@
-<script setup>
-import { RouterLink } from 'vue-router'
-</script>
-
 <template>
   <nav class="nav-container">
-    <div>
-      <h3>Logo</h3>
+    <div id="logo-container">
+      <h3 id="logo-image">Logo</h3>
     </div>
-    <RouterLink class="link-name" to="/">Inicio</RouterLink>
-    <RouterLink class="link-name" to="/about">Nosotros</RouterLink>
-    <button class="menu-bars-container">
-      <i class="pi pi-bars"></i>
+
+    <button @click="openNavBarPanel" class="open-navbar-menu-button">
+      <i class="pi pi-bars" style="font-size: 20px;"></i>
     </button>
-    <!-- <i class="pi pi-times"></i> -->
+
+    <div id="nav-items-desktop-container">
+      <div id="nav-items-desktop-box">
+        <RouterLink @click="closeNavBarPanel" class="nav-item" to="/">Inicio</RouterLink>
+        <RouterLink @click="closeNavBarPanel" class="nav-item" to="/about">Nosotros</RouterLink>
+        <RouterLink @click="closeNavBarPanel" class="nav-item" to="/calculator">Calculadora de M2</RouterLink>
+      </div>
+    </div>
+
+    <Teleport to="body">
+      <Transition>
+        <div v-if="isNavBarPanelOpen" id="navbar-menu-container">
+          <div id="close-navbar-menu-button-container">
+            <button id="close-navbar-menu-button" @click="closeNavBarPanel">
+              <i class="pi pi-times"></i>
+            </button>
+          </div>
+
+          <div id="nav-items-mobile-container">
+            <RouterLink @click="closeNavBarPanel" class="nav-item" to="/">Inicio</RouterLink>
+            <RouterLink @click="closeNavBarPanel" class="nav-item" to="/about">Nosotros</RouterLink>
+            <RouterLink @click="closeNavBarPanel" class="nav-item" to="/calculator">Calculadora de M2</RouterLink>
+          </div>
+        </div>
+      </Transition>
+    </Teleport>
   </nav>
 </template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+import { ref } from 'vue'
+
+const isNavBarPanelOpen = ref(false);
+
+function openNavBarPanel() {
+  isNavBarPanelOpen.value = true;
+}
+function closeNavBarPanel() {
+  isNavBarPanelOpen.value = false;
+}
+</script>
 
 <style scoped>
 .nav-container {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  position: sticky ;
+  position: sticky;
   top: 0;
   width: 100%;
   height: 7vh;
-  border: 1px solid red;
-  padding-left: 10px;
-  padding-right: 10px;
+  font-family: Roboto;
+  background: #fff;
 }
-.menu-bars-container {
+.open-navbar-menu-button {
   display: flex;
   justify-content: center;
   align-items: center;
   width: 10%;
   height: 70%;
-  border: 1px solid black;
+  /* border: 1px solid black; */
+  border: none;
+  background: none;
   border-radius: 5px;
 }
-.link-name {
+@media screen and (width > 768px) {
+  .open-navbar-menu-button {
+    display: none;
+  }
+}
+#navbar-menu-container {
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 40%;
+  /* border: 1px solid red; */
+  background: #fff;
+}
+#close-navbar-menu-button-container {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  width: 100%;
+  height: 20%;
+  padding-right: 10px;
+}
+#close-navbar-menu-button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 10%;
+  height: 70%;
+  border: 1px solid #000;
+  border-radius: 5px;
+  background: transparent;
+}
+#nav-items-mobile-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 100%;
+  height: 80%;
+}
+#nav-items-desktop-container {
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+  height: 100%;
+  border: 1px solid red;
+}
+#nav-items-desktop-box {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  width: 60%;
+  height: 100%;
+  border: 1px solid blue;
+}
+@media screen and (width < 769px) {
+  #nav-items-desktop-container {
+    display: none;
+  }
+}
+#logo-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 20%;
+  height: 100%;
+}
+.nav-item {
   text-decoration: none;
+}
+
+/* TRANSITION CLASSES COMPONENT */
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
 }
 </style>
