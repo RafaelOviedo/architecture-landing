42256a60ab3a8b87265b258bd101588290216163
Rafael Oviedo <rafaeloviedo@Rafaels-Air.fibertel.com.ar>
2024-03-09 02:33:39 -0300

    update: adding ProjectDetail view and adding structure to projects list

---
+++ src/views/ProjectDetail.vue
@@ -0,0 +1,57 @@
+<template>
+  <main class="projects-component">
+    <div>
+      <TabMenu v-model:activeIndex="active" :model="items" />
+
+      <Galleria :value="images" :responsiveOptions="responsiveOptions" :numVisible="5" containerStyle="max-width: 640px">
+        <template #item="slotProps">
+            <img :src="slotProps.item.itemImageSrc" :alt="slotProps.item.alt" style="width: 100%" />
+        </template>
+        <template #thumbnail="slotProps">
+            <img :src="slotProps.item.thumbnailImageSrc" :alt="slotProps.item.alt" />
+        </template>
+      </Galleria>
+
+      <Button label="Ver Fotos" @click="displayBasic = true" />
+    </div>
+  </main>
+</template>
+
+<script setup>
+import Button from 'primevue/button';
+import TabMenu from 'primevue/tabmenu';
+import Galleria from 'primevue/galleria';
+import { ref } from "vue";
+
+const images = ref();
+const displayBasic = ref(false);
+const responsiveOptions = ref([
+    {
+        breakpoint: '1300px',
+        numVisible: 4
+    },
+    {
+        breakpoint: '575px',
+        numVisible: 1
+    }
+]);
+
+const active = ref(0);
+
+const items = ref([
+  { label: 'Diseño', icon: '' },
+  { label: 'Construccion', icon: '' },
+  { label: 'Resultado', icon: '' }
+]);
+</script>
+
+<style>
+.projects-component {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 600px;
+  border: 1px solid blue;
+}
+</style>
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,18 +1,94 @@
-<script setup>
-</script>
-
 <template>
   <main class="projects-component">
-    Proyectos
+    <div id="projects-title-container">
+      <h2>Proyectos</h2>
+    </div>
+
+    <div id="projects-container">
+      <div class="project-box">
+        <div id="project-thumbnail-container">
+          Imagen
+        </div>
+
+        <div id="project-title-container">
+          <h3>Proyecto 1</h3>
+        </div>
+      </div>
+      <div class="project-box">
+        <div id="project-thumbnail-container">
+          Imagen
+        </div>
+
+        <div id="project-title-container">
+          <h3>Proyecto 2</h3>
+        </div>
+      </div>
+      <div class="project-box">
+        <div id="project-thumbnail-container">
+          Imagen
+        </div>
+
+        <div id="project-title-container">
+          <h3>Proyecto 3</h3>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
+
+<script setup>
+</script>
 
 <style>
 .projects-component {
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   width: 100%;
-  height: 700px;
+  height: 93vh;
+}
+#projects-title-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 10%;
+  width: 100%;
+}
+#projects-container {
+  height: 90%;
+  width: 100%;
+  /* border: 1px solid blue; */
+  overflow: scroll;
+  overflow-x: hidden;
+  padding-left: 10%;
+}
+.project-box {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  width: 90%;
+  height: 60%;
+  margin-bottom: 30px;
+  border-radius: 5px;
+  cursor: pointer;
+  /* border: 1px solid #000; */
+  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.3);
+}
+#project-thumbnail-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 70%;
+  width: 90%;
+  border: 1px solid red;
+}
+#project-title-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 20%;
+  width: 90%;
 }
 </style>
