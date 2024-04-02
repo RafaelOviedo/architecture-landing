0c04d02a59e84c6272d225f003eecfa11cf5611d
Rafael Oviedo <rafaeloviedo@Rafaels-Air.fibertel.com.ar>
2024-04-02 00:04:41 -0300

    update: adding first structure for m2 calculator

--- src/views/CalculatorView.vue
+++ src/views/CalculatorView.vue
@@ -1,18 +1,70 @@
-<script setup>
-</script>
 
 <template>
   <main class="calculator-component">
-    M2 Calculator
+    <div id="title-text-container">
+      <h2>Welcome to the Architectural Cost Calculator</h2>
+      <br />
+      <span>
+        Our calculator simplifies budget estimation for architectural projects based on square meter measurements. 
+        Input your area dimensions and customize costs for materials, labor, and additional expenses. 
+        Instantly calculate total project costs and visualize breakdowns. Accessible on-the-go and compatible with international projects.
+        <br />
+        <br />
+        Start planning with confidence today!
+      </span>
+    </div>
+
+    <form id="calculator-container">
+      <div id="currency-selector-container">
+        <MultiSelect v-model="selectedCurrency" :options="currencies" optionLabel="name" placeholder="Currency" :maxSelectedLabels="3" class="w-full md:w-20rem" />
+      </div>
+    </form>
   </main>
 </template>
+
+<script setup>
+import MultiSelect from 'primevue/multiselect';
+import { ref } from "vue";
+
+const selectedCurrency = ref();
+const currencies = ref([
+    { name: '$' },
+    { name: '€' }
+]);
+</script>
 
 <style>
 .calculator-component {
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   width: 100%;
-  height: 700px;
+  height: 1000px;
+}
+#title-text-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 90%;
+  height: 30%;
+}
+#calculator-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100%;
+  height: 70%;
+  border: 1px solid black;
+}
+#currency-selector-container {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  width: 95%;
+  height: 10%;
+  border-bottom: 1px solid #000;
 }
 </style>
